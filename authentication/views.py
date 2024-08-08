630ac425c8eb1c525f237303dee17afbc7c8f1f3
Leonardo Amaral <leomonteiro05@gmail.com>
2024-08-08 08:00:52 -0300

    login

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 
@@ -37,3 +38,37 @@ class RegistrationView(View):
             user.save()
             messages.success(request, 'A sua conta foi criada')
             return render(request,'authentication/register.html')
+        
+class LoginView(View):
+    def get(self,request):
+        return render(request, 'authentication/login.html')
+    
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if username and password:
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                if user.is_active:
+                    auth.login(request, user)
+                    messages.success(request, 'Bem-vindo, '+user.username+'.')
+
+                    return redirect('recordsapp')
+                messages.error(
+                    request, 'A conta não existe, por favor solicite a criação de um acesso')
+                return render(request, 'authentication/login.html')
+            messages.error(
+                request, 'Credenciais inválidas, tente novamente')
+            return render(request, 'authentication/login.html')
+        messages.error(
+            request, 'Por favor, preencha todos os campos')
+        return render(request, 'authentication/login.html')
+    
+class LogoutView(View):
+    def post(self, request):
+        auth.logout(request)
+        messages.success(request, 'Você foi desconectado')
+        return redirect('login')
+
--- recordsapp/views.py
+++ recordsapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url='/authentication/login')
 def index(request):
     return render(request,'records/index.html')
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url='/authentication/login')
 def add_record(request):
     return render(request,'records/add_record.html')
 
