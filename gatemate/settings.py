e2e5c36d31d7ee407652948cd5dd86ff1b2def5b
Leonardo Amaral <leomonteiro05@gmail.com>
2024-08-29 08:23:22 -0300

    settings changes

--- gatemate/settings.py
+++ gatemate/settings.py
@@ -15,6 +15,8 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from django.contrib import messages
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +31,7 @@ SECRET_KEY = 'django-insecure-ykjls)8xdbokw452^v)1+81m120wc7-_ro^pe5@0x*gpeo#k)d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "GateMate.pythonanywhere.com"]
 
 
 # Application definition
@@ -81,13 +83,9 @@ WSGI_APPLICATION = 'gatemate.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_USER_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 
